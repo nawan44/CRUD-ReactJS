36d987a0f505a064dd81177d72ccddb74de2dd4e
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-09-21 23:29:23 +0700

    run build

---
+++ build/precache-manifest.971fea82224585b372522e4d1aae2ca7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747880b69dfe0acd06d214ffc6ec0e78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d1d111d9edaf6dc8ad7",
+    "url": "/static/css/2.373572e1.chunk.css"
+  },
+  {
+    "revision": "946d8359693be3a9f4ba",
+    "url": "/static/css/main.2aa5466d.chunk.css"
+  },
+  {
+    "revision": "2d1d111d9edaf6dc8ad7",
+    "url": "/static/js/2.7a855796.chunk.js"
+  },
+  {
+    "revision": "496d266235ab5acdf6217f7d39702561",
+    "url": "/static/js/2.7a855796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946d8359693be3a9f4ba",
+    "url": "/static/js/main.705c85b3.chunk.js"
+  },
+  {
+    "revision": "0c1cc5c937827a6b1b95",
+    "url": "/static/js/runtime-main.26591230.js"
+  }
+]);
--- src/component/config/config.jsx
+++ src/component/config/config.jsx
@@ -1,3 +1,3 @@
-export const config = {
-    apiUrl: 'http://localhost:8080/api/todo'
-}
+// export const config = {
+//     apiUrl: 'http://localhost:8080/api/todo'
+// }
